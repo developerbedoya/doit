6ebcc9d682389994b0df4061d1be43157a52a94f
Daniel Bedoya Alzate <dbalzate@gmail.com>
2023-08-12 23:38:27 -0300

    Detectar vários sistemas (Linux deb ou rpm, Windows e Mac)

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ namespace DoIt;
 
 public class Program
 {
+    private static string _commandPrompt;
+    private static string _commandArguments;
+    private static string _system;
     public static async Task Main(string[] args)
     {
         // Antes de executar este programa, colocar a chave de API no arquivo .openai
@@ -17,6 +20,8 @@ public class Program
         Console.WriteLine("Digite o que você deseja fazer e eu vou tentar te ajudar.");
         Console.WriteLine("Para sair, basta pressionar ENTER sem digitar nada.");
         Console.WriteLine();
+
+        DetectEnvironment();
         
         while (true)
         {
@@ -30,7 +35,7 @@ public class Program
             
             var mensagens = new List<Message>
             {
-                new Message(Role.System, "You provide terminal prompts for Windows users. Respond with a CMD command only on one line with no explanatory text."),
+                new Message(Role.System, $"You provide terminal prompts for ${_system} users. Respond with a ${_commandPrompt} command only on one line with no explanatory text."),
                 new Message(Role.User, desejo),
             };
             
@@ -38,7 +43,81 @@ public class Program
             var result = await api.ChatEndpoint.GetCompletionAsync(chatRequest);
 
             var comando = result.Choices.FirstOrDefault()?.Message;
-            Process.Start("cmd.exe", $"/c {comando}");
+            Process.Start(_commandPrompt, string.Format(_commandArguments, comando));
         }
+    }
+
+    static void DetectEnvironment()
+    {
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        {
+            _system = "Windows";
+            _commandPrompt = "cmd.exe";
+            _commandArguments = "/c {0}";
+        }
+        else if (Environment.OSVersion.Platform == PlatformID.Unix)
+        {
+            if (IsRpmBasedSystem())
+            {
+                _system = "RPM-based Linux";
+                _commandPrompt = "/bin/bash";
+                _commandArguments = "-c \"{0}\"";
+            }
+            else if (IsDebBasedSystem())
+            {
+                _system = "DEB-based Linux";
+                _commandPrompt = "/bin/bash";
+                _commandArguments = "-c \"{0}\"";
+            }
+            else
+            {
+                _system = "Linux";
+                _commandPrompt = "/bin/bash";
+                _commandArguments = "-c \"{0}\"";
+            }
+        }
+        else if (Environment.OSVersion.Platform == PlatformID.MacOSX)
+        {
+            _system = "MacOS";
+            _commandPrompt = "/bin/bash";
+            _commandArguments = "-c \"{0}\"";
+        }
+        else
+        {
+            Console.WriteLine("Running on unknown OS");
+            Environment.Exit(1);
+        }
+    }
+    
+    static bool IsRpmBasedSystem()
+    {
+        // Check for RPM-related files or directories
+        string[] rpmSpecificPaths = { "/usr/bin/rpm", "/etc/redhat-release" };
+
+        foreach (string path in rpmSpecificPaths)
+        {
+            if (File.Exists(path) || Directory.Exists(path))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static bool IsDebBasedSystem()
+    {
+        // Check for DEB-related files or directories
+        string[] debSpecificPaths = { "/usr/bin/dpkg", "/etc/debian_version" };
+
+        foreach (string path in debSpecificPaths)
+        {
+            if (File.Exists(path) || Directory.Exists(path))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
